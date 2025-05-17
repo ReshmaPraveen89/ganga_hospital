c08baaeca35ae77f62efd0b5db747f2457bd3d0d
Reshma praveen <reshupraveen89@gmail.com>
2025-05-17 16:58:03 +0530

    add changes

--- pricing.php
+++ pricing.php
@@ -79,7 +79,7 @@
         <div class="row no-gutters slider-text align-items-center justify-content-center">
           <div class="col-md-9 ftco-animate text-center">
             <h1 class="mb-2 bread">Pricing</h1>
-            <p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Pricing <i class="ion-ios-arrow-forward"></i></span></p>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.phpgit checkout">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Pricing <i class="ion-ios-arrow-forward"></i></span></p>
           </div>
         </div>
       </div>
